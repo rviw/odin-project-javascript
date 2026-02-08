efda5665c50aad4873cce83e776c4aedaae643b7
rviw <216364371+rviw@users.noreply.github.com>
2026-02-08 11:37:10 +0900

    feat: implement iterative fibonacci

--- projects/recursion/fibonacci.js
+++ projects/recursion/fibonacci.js
@@ -1,5 +1,14 @@
-export function fibs() {
-  throw new Error("Not Implemented");
+export function fibs(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+
+  const result = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    result.push(result[i - 2] + result[i - 1]);
+  }
+
+  return result;
 }
 
 export function fibsRec() {
