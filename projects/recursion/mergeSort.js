d38a2122a43366136857273a47be4a9b5129d171
rviw <216364371+rviw@users.noreply.github.com>
2026-02-08 11:57:41 +0900

    feat: implement recursive merge sort

--- projects/recursion/mergeSort.js
+++ projects/recursion/mergeSort.js
@@ -1,3 +1,26 @@
-export function mergeSort() {
-  throw new Error("Not implemented");
+const merge = (left, right) => {
+  const result = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i++]);
+    } else {
+      result.push(right[j++]);
+    }
+  }
+
+  return [...result, ...left.slice(i), ...right.slice(j)];
+};
+
+export function mergeSort(array) {
+  if (array.length <= 1) return array;
+
+  const mid = Math.floor(array.length / 2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+
+  return merge(left, right);
 }
