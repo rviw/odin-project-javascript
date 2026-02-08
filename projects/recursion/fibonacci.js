f9fd2680b49155d2ede0b29977e6b2cd113978d7
rviw <216364371+rviw@users.noreply.github.com>
2026-02-08 11:44:22 +0900

    feat: implement recursive fibonacci

--- projects/recursion/fibonacci.js
+++ projects/recursion/fibonacci.js
@@ -11,6 +11,16 @@ export function fibs(n) {
   return result;
 }
 
-export function fibsRec() {
-  throw new Error("Not Implemented");
+export function fibsRec(n) {
+  // console.log("This was printed recursively");
+
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const prev = fibsRec(n - 1);
+
+  return [...prev, prev.at(-2) + prev.at(-1)];
 }
+
+// fibsRec(8);
